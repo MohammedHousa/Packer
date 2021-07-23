6d3ff85f7baef28e8e3c5bc3fd887bccfe8aa177
MohammedHousa <78443364+MohammedHousa@users.noreply.github.com>
2021-07-23 22:04:01 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = ""
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_instance" "server1" {
+  ami           = "ami-0821a7f11cb808cc6"
+  instance_type = "t2.micro"
+  key_name      = "virg"
+
+  tags = {
+    Name = "from-image"
+  }
+}
